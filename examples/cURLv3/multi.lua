7efaba4ff23e3918f4ae9cac6d72a9f3e236e22a
Alexey Melnichuk <mimir@newmail.ru>
2014-09-15 13:20:08 +0500

    Update doc[ci skip]

--- examples/cURLv3/multi.lua
+++ examples/cURLv3/multi.lua
@@ -12,6 +12,6 @@ local f1 = io.open("lua.html",    "w+b")
 local f2 = io.open("luajit.html", "w+b")
 
 for data, type, easy in m:iperform() do
-    if type == "data" and c1 == easy then f1:write(data) end
-    if type == "data" and c2 == easy then f2:write(data) end
+  if type == "data" and c1 == easy then f1:write(data) end
+  if type == "data" and c2 == easy then f2:write(data) end
 end

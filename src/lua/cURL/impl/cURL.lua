cf41acae7b19e7c663ef4882b755fe35c9d27d55
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2016-05-04 13:59:47 +0300

    Back to developer version. [ci skip]

--- src/lua/cURL/impl/cURL.lua
+++ src/lua/cURL/impl/cURL.lua
@@ -10,7 +10,7 @@
 
 local module_info = {
   _NAME      = "Lua-cURL";
-  _VERSION   = "0.3.4";
+  _VERSION   = "0.3.5-dev";
   _LICENSE   = "MIT";
   _COPYRIGHT = "Copyright (c) 2014-2016 Alexey Melnichuk";
 }

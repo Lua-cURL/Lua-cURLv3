350346c9a821819304afe6714f4630f2ddf6726e
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2017-04-11 14:35:15 +0300

    Back to dev version [ci skip]

--- src/lua/cURL/impl/cURL.lua
+++ src/lua/cURL/impl/cURL.lua
@@ -1,7 +1,7 @@
 --
 --  Author: Alexey Melnichuk <alexeymelnichuck@gmail.com>
 --
---  Copyright (C) 2014-2016 Alexey Melnichuk <alexeymelnichuck@gmail.com>
+--  Copyright (C) 2014-2017 Alexey Melnichuk <alexeymelnichuck@gmail.com>
 --
 --  Licensed according to the included 'LICENSE' document
 --
@@ -10,9 +10,9 @@
 
 local module_info = {
   _NAME      = "Lua-cURL";
-  _VERSION   = "0.3.6";
+  _VERSION   = "0.3.7-dev";
   _LICENSE   = "MIT";
-  _COPYRIGHT = "Copyright (c) 2014-2016 Alexey Melnichuk";
+  _COPYRIGHT = "Copyright (c) 2014-2017 Alexey Melnichuk";
 }
 
 local function clone(t, o)

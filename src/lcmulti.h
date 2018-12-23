c8cd51eb856d185a9fa6b7bbfc3b43d4bfc03e4c
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2018-12-23 16:27:09 +0300

    Update email in copyright comments

--- src/lcmulti.h
+++ src/lcmulti.h
@@ -1,11 +1,11 @@
 /******************************************************************************
-* Author: Alexey Melnichuk <mimir@newmail.ru>
+* Author: Alexey Melnichuk <alexeymelnichuck@gmail.com>
 *
-* Copyright (C) 2014 Alexey Melnichuk <mimir@newmail.ru>
+* Copyright (C) 2014-2018 Alexey Melnichuk <alexeymelnichuck@gmail.com>
 *
 * Licensed according to the included 'LICENSE' document
 *
-* This file is part of lua-lcurl library.
+* This file is part of Lua-cURL library.
 ******************************************************************************/
 
 #ifndef _LCMULTI_H_

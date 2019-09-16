c38f3d4c298a1ce941158e0b8bc9ea5ca40fe655
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2019-09-16 10:26:55 +0300

    Release 0.3.11

--- src/lua/cURL/impl/cURL.lua
+++ src/lua/cURL/impl/cURL.lua
@@ -10,7 +10,7 @@
 
 local module_info = {
   _NAME      = "Lua-cURL";
-  _VERSION   = "0.3.10";
+  _VERSION   = "0.3.11";
   _LICENSE   = "MIT";
   _COPYRIGHT = "Copyright (c) 2014-2019 Alexey Melnichuk";
 }
